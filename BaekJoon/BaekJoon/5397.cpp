90f5dbb7510f537f3070ea00e84f688d6ed49b90
허지용 <hjy2501@naver.com>
2019-07-18 17:22:10 +0900

    백준 2346 ing

--- BaekJoon/BaekJoon/5397.cpp
+++ BaekJoon/BaekJoon/5397.cpp
@@ -1,63 +1,51 @@
 #include <iostream>
-#include <string>
-#include <stack>
+#include <algorithm>
+#include <deque>
 using namespace std;
-stack <char> st1, st2;
-void move_left()
-{
-	if (!st1.empty()){
-		st2.push(st1.top());
-		st1.pop();
-	}
-}
-void move_right()
-{
-	if (!st2.empty()){
-		st1.push(st2.top());
-		st2.pop();
-	}
-}
-void del()
-{
-	if (!st1.empty())
-		st1.pop();
-}
-void back_result()
-{
-	while (!st1.empty()){
-		st2.push(st1.top());
-		st1.pop();
-	}
-	while (!st2.empty()){
-		 cout << st2.top();
-		 st2.pop();
-	}
-	cout << endl;
-}
+
+deque <int> dq1;
+deque <int> dq2;
 
 int main()
 {
-	//freopen("input.txt","r",stdin);
-	int tc;
-	string temp;
-	cin >> tc;
+	freopen("input.txt", "r", stdin);
+	int N;
+	int temp;
+	int index = 1;
+	int move = 0;
+	cin >> N;
+	for (int i = 0; i < N; i++)	cin >> temp, dq1.push_back(temp);
 
-	for (int i = 0; i < tc; i++)
+	while (!dq2.empty())
 	{
-		cin >> temp;
-		for (int j = 0; j < temp.length(); j++)
+		move = 0;
+		if (abs(dq1.front() >= dq1.size()))
+			move = dq1.front() % dq1.size();
+		else
+			move = dq1.front();
+
+		if (dq1.front() < 0)	//move left
 		{
-			if (temp[j] == '<')
-				move_left();
-			else if (temp[j] == '>')
-				move_right();
-			else if (temp[j] == '-')
-				del();
-			else {
-				st1.push(temp[j]);
+			for (int i = 0; i < move; i++)
+			{
+				dq2.push_front(dq1.);
 			}
 		}
-		back_result();
+		else if (dq1.front() > 0)	//move right
+		{
+			for (int i = 0; i < move; i++)
+			{
+				dq2.push_back(dq1.front());
+				dq1.pop_front();
+			}
+			dq1.pop_front();
+		}
+		while (!dq2.empty())	//다시 dq1 을 제자리로
+		{
+			dq1.push_back(dq2.front());
+			dq2.pop_front();
+		}
 	}
+
 	return 0;
 }
--- TestProject/TestProject/소스.cpp
+++ TestProject/TestProject/소스.cpp
@@ -1,29 +1,43 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
+#include <deque>
 using namespace std;
-priority_queue <int> pq;
-pair <int, int> homework[1000];
-int n, sum, date;
+deque <pair<int,int>> mq;
+deque <pair<int,int>> tq;
+
+int init_q()
+{
+	while(!tq.empty()){
+		mq.push_back(tq.front());
+		tq.pop_front();
+	}
+	return 0;
+}
 int main()
 {
 	//freopen("input.txt","r",stdin);
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> homework[i].first >> homework[i].second;
-	sort(homework, homework + n);
-	for (int i = 0; i < n; i++){
-		if (date == homework[i].first){
-			date--;
-			pq.pop();
+	int N;
+	int move;
+	pair<int, int>temp;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> temp.second;
+		temp.first = i + 1;
+		mq.push_back(temp);
+	}
+
+	while (!mq.empty()){
+		cout << mq.front().first;
+		move = abs(mq.front().second) % mq.size()-1;
+		if ((mq.front().second) < 0)	move = mq.size()-1 - move;
+		mq.pop_front();
+		if (mq.empty())
+			break;
+		for (int i = 0; i < move; i++){
+			tq.push_back(mq.front());
+			mq.pop_front();
 		}
-		pq.push(-homework[i].second);
-		date++;
+		init_q();
 	}
-	while(!pq.empty()){
-		sum += (-pq.top());
-		pq.pop();
-	}
-	cout << sum;
 	return 0;
 }
