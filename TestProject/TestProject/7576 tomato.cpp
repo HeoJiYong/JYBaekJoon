0e0646b53742b3fa0f2db152ea456c5ff29a5592
허지용 <hjy2501@naver.com>
2020-03-19 19:59:49 +0900

    2178 미로찾기 추가

---
+++ TestProject/TestProject/7576 tomato.cpp
@@ -0,0 +1,76 @@
+//2178 미로찾기
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+int mx[] = {-1,0,1,0};
+int my[] = {0,1,0,-1};
+int map[101][101];
+
+struct node {
+	int x=0;
+	int y=0;
+	int count=1;
+};
+
+int N, M;
+queue <node> q;
+
+bool is_safe(int x, int y) {
+	return ((x >= 0) && (y >= 0) && (x < N) && (y < M));
+}
+
+int bfs(){
+	int qsize = 0;
+	int x, y,count=1;
+	map[0][0] = 0;
+	while (!q.empty())
+	{
+		qsize = q.size();
+		for (int i = 0; i < qsize; i++)
+		{
+			if ((q.front().x == N - 1) && (q.front().y == M - 1)) {
+				cout <<q.front().count;
+				return 0;
+			}
+			//큐 맨앞 노드 기준으로
+			//4방향 검사 (1. 안전한지 2.'1'인지 3.방문한적 있는지)
+			count =q.front().count;
+			for (int j = 0; j < 4; j++)
+			{
+				x = q.front().x + mx[j];
+				y = q.front().y + my[j];
+				if (is_safe(x ,y))
+				{
+					if (map[x][y]==1)
+					{
+						//위 검사 다 통과하면 해당 노드 큐에 삽입.
+						//(삽입시 x,y정보와 + 큐의 맨앞노드의 count+1한 값)
+						q.push({ x ,y ,count+1});
+						map[x][y] = 0;
+					}
+				}
+			}
+			q.pop();
+		}
+	}
+	return 0;
+}
+
+int main() 
+{
+	string temp;
+	q.push({});
+	cin >> N>> M;
+	for (int i = 0; i < N; i++)
+	{
+		temp = "";
+		cin >> temp;
+		for (int j = 0; j < M; j++)
+		{
+			map[i][j] = temp[j] - '0' ;
+		}
+	}
+	bfs();
+}
--- TestProject/TestProject/소스2.cpp
+++ TestProject/TestProject/소스2.cpp
@@ -1,49 +1,43 @@
 #include <iostream>
-#include <queue>
-#include <vector>
+#include <string>
+#include <cmath>
 using namespace std;
-
-vector<int> tree[10];
-bool visit[10];
-void bfs(int start)
-{
-	queue<int> q;
-	q.push(start);
-	visit[start] = true;
-	while (!q.empty()) {
-		int x = q.front();
-		q.pop();
-		printf("%d ",x);
-		for(int i =0; i<x.)
-	}
-}
 
 int main()
 {
-	tree[1].push_back(2);
-	tree[1].push_back(3);
-
-	tree[2].push_back(1);
-	tree[2].push_back(4);
-	tree[2].push_back(5);
-
-	tree[3].push_back(1);
-	tree[3].push_back(6);
-	tree[3].push_back(7);
-
-	tree[4].push_back(2);
-	tree[4].push_back(8);
-	tree[4].push_back(9);
-
-	tree[5].push_back(2);
-	tree[5].push_back(10);
-
-	tree[6].push_back(3);
-	tree[7].push_back(3);
-	tree[8].push_back(4);
-	tree[9].push_back(4);
-	tree[10].push_back(5);
-
-	bfs(1);
+	/*
+	int temp = 0b0001;	//2진수로 넣기
+	printf("%");
+	*/
+	/*
+	int n = 10;
+	int a = 9;
+	for (int i = n; i >= 0; i--){
+		printf("%d",(a>>i)&1);
+	}
+	return 0;
+	*/ 
+	long long int N, K;
+	cin >> N >> K;
+	long long int flag = N / 2;
+	long long int pattern1 = 1, pattern2 = 1;
+	if (N % 2 == 0) {
+		for (long long int i = 0; i < flag - 1; i++) {
+			pattern1 = (pattern1 * 2) + 1;
+			pattern2 = (pattern2 * 4) + 1;
+		}
+		if (K >= pattern1)	K++;
+		if (K >= pattern2)	K++;
+	}
+	if (K >= pow(2, N)) {
+		cout << "-1";
+		return 0;
+	}
+	for (long long int i = N - 1; i >= 0; i--) {
+		if (((K >> i) & 1))
+			cout << ")";
+		else
+			cout << "(";
+	}
 	return 0;
 }
